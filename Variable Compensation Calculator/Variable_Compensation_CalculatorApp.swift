737fbff6f3a3747701f2cb424e3fc37ffe2779ff
cgaaf <cgaafary@gmail.com>
2023-10-02 19:55:51 -0400

    Initial Commit

---
+++ Variable Compensation Calculator/Variable_Compensation_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Variable_Compensation_CalculatorApp.swift
+//  Variable Compensation Calculator
+//
+//  Created by Chris Gaafary on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Variable_Compensation_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
